1ef1217a56255cdec798de797446e17f13de31e7
Raghav <ra346238@gmail.com>
2025-03-17 20:21:22 -0700

    Navbar layout added

--- focusforge/src/mainP.js
+++ focusforge/src/mainP.js
@@ -1,14 +1,40 @@
-const mainP = () => {
+import React from 'react';
+// import './MainP.css'
+import logout from './MPimgs/logout.png';
+const NavBar = () => {
 
     return (
-        <div className = "container">
+        <nav className = "navBar">
+        <ul className='focusF'>
+            <li>FocusForge</li>
+        </ul>
+        <div>
+        <ul className='themes'>
+            <li>Themes</li>
+        </ul>
+        <img src = {logout}></img>
+        </div>
+        </nav>
+    );
+}
+
+
+
+const MainP = () => {
+
+
+    return (
+        <div className = "mainP">
+        {/* <h1>MAIN PAGE</h1> */}
+        <NavBar />
 
 
 
 
-            
         </div>
     )
 
 
 }
+
+export default MainP;
