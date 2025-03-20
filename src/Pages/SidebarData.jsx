cd97d30ebb3beb1f317d504aced7359a2da38ce6
Raghav <ra346238@gmail.com>
2025-03-19 21:05:33 -0700

    Sidebar data added

---
+++ src/Pages/SidebarData.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const SidebarData = [
+    {
+        title: "music",
+        icon: <Icon />,
+        link: "/music"
+    },
+    {
+        title: "stickyNotes",
+        icon: <Icon />,
+        link: "/stickyN"
+    },
+    {
+        title: "toDoList",
+        icon: <Icon />,
+        link: "/toDoL"
+    }
+];
+
+
+export default SidebarData
