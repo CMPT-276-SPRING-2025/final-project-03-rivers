6c5d12aef1cb603cfb42c5961582975f66b29d8f
Laraib <laraib.yousfani@gmail.com>
2025-03-31 16:11:48 -0700

    Set up state hook and implemented add function for sticky notes

--- src/Pages/Components/StickyNotes.jsx
+++ src/Pages/Components/StickyNotes.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const StickyNotes = () => {
-    return <div>Sticky Notes</div> 
+    {/*Create a state hook to manage sticky notes*/}
+    const [notes, setNotes] = useState([]);
+
+    {/*Function to create sticky notes*/}
+    const createNote = () => {
+        const newNote = {
+            id: Date.now(), 
+            text: '',
+        };
+        setNotes([...notes, newNote]);
+    }
 }
 
 export default StickyNotes;
