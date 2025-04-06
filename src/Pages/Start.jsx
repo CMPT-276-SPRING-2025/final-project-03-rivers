7ce610a81dd2bbe9973e78566b2eca3088d1bb2b
Laraib <laraib.yousfani@gmail.com>
2025-04-06 11:57:39 -0700

    Added redirection and authentication (frontend) for todoist.

--- src/Pages/Start.jsx
+++ src/Pages/Start.jsx
@@ -20,6 +20,19 @@ const Start = () => {
     exit: { x: -300, opacity: 0, transition: { duration: 0.5 } },
   };
 
+  const generateState = () => Math.random().toString(36).substring(2, 15);
+  
+  const handleTodoist = () => {
+    const clientid = import.meta.env.VITE_TODOIST_CLIENT_ID;
+    const redirect_uri = import.meta.env.VITE_TODOIST_REDIRECT_URI;
+    const state = generateState();
+
+    const authURL =  `https://todoist.com/oauth/authorize?client_id=${clientid}&scope=data:read_write&state=${state}&redirect_uri=${redirect_uri}&response_type=code&force_confirm=true`;
+
+    window.location.href = authURL;
+  } 
+
+
   return (
     <div className="start-page">
       <div className="top-nav-bar" data-testid="nav-bar">
@@ -64,9 +77,10 @@ const Start = () => {
                 className="logo-container"
               >
                 <h1 className="gradient-text">Connect to Todoist</h1>
-                <button className="start-button" onClick={handleContinue}>
+                <button className="start-button" onClick={handleTodoist}>
                   Connect To Todoist →
                 </button>
+                <h4>Connect To Todoist And You Will Be Redirected To Our Application</h4>
               </motion.div>
             )}
           </AnimatePresence>
