af9e1d8c743f3d72b4f7fce302fdcbc66e3c279c
Laraib <laraib.yousfani@gmail.com>
2025-04-06 09:47:42 -0700

    Finished styling start page and created UI.

--- src/Pages/Start.jsx
+++ src/Pages/Start.jsx
@@ -1,38 +1,17 @@
 import React from "react";
 import "./start.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import {useNavigate} from "react-router-dom";
 
 const Start = () => {
-  const floatingStars = Array.from({ length: 12 }); // Array of 12 stars 
-
   return (
     <div className="start-page">
-      <h1>Welcome to the Start Page!</h1>
-
-      {floatingStars.map((_, i) => {
-
-        // randomize size and duration of the stars
-        const size = 20 + Math.random() * 40;
-        const duration = 15 + Math.random() * 10;
-
-        // Distribute the stars across the screen
-        const left = `${Math.random() * 50 + (i < 6 ? 0 : 50)}%`;
-
-        return (
-          <FontAwesomeIcon
-            key={i}
-            icon={faStar}
-            className="floating-icon"
-            style={{
-              left,
-              fontSize: `${size}px`,
-              animationDuration: `${duration}s`,
-              animationDelay: `${Math.random() * 10}s`,
-            }}
-          />
-        );
-      })}
+      <div className="overlay">
+        <div className="logo-container">
+          <h1 className = "gradient-text">FocusForge</h1>
+        </div>
+        <h3>The Solution To Procrastination!</h3>
+        <button className="start-button">Continue →</button>
+      </div>
     </div>
   );
 };
