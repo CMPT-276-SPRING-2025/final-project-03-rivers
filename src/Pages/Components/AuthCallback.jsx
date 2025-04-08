1008400e1368dec12b823d64c9dda3dbe2f84123
Akira Egawa <lucian.ming.chen@gmail.com>
2025-04-07 19:42:15 -0700

    Fixed Laraib's illiteracy

--- src/Pages/Components/AuthCallback.jsx
+++ src/Pages/Components/AuthCallback.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./AuthCallBack.css";
+import "./AuthCallback.css";
 import question from "../../assets/question.png";
 import logo from "../../assets/logo.png";
 
