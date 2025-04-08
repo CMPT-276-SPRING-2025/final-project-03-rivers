70885b34468b443a25292c7fb137a42b4bfcfd0a
Akira Egawa <lucian.ming.chen@gmail.com>
2025-04-07 22:16:09 -0700

    reverted removal of VITE

--- src/Pages/Components/Model.jsx
+++ src/Pages/Components/Model.jsx
@@ -1,7 +1,7 @@
 // components/Model.jsx
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(import.meta.env.REACT_APP_API_KEY);
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_REACT_APP_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export const generateContent = async (prompt) => {
