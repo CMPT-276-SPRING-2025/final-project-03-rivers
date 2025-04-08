f67de27411f447030fd7c0b7d5866e2556efa761
Akira Egawa <lucian.ming.chen@gmail.com>
2025-04-07 21:36:47 -0700

    Trying to fix backend

--- src/Pages/Components/AuthCallback.jsx
+++ src/Pages/Components/AuthCallback.jsx
@@ -36,7 +36,7 @@ const AuthCallback = () => {
   
     if (code) {
       axios
-        .post("http://localhost:4000/auth/token", { code })
+        .post("/api/auth-token", { code })
         .then((res) => {
           localStorage.setItem("access_token", res.data.access_token);
           setMessage("Authentication successful!");
