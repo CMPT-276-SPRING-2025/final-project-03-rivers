a6147a92e9952e80025fc16b0af6c50d0fd2ece1
Akira Egawa <lucian.ming.chen@gmail.com>
2025-04-07 19:47:50 -0700

    Revert "Fixed Laraib's illiteracy"
    This reverts commit 1008400e1368dec12b823d64c9dda3dbe2f84123.

--- src/Pages/Components/AuthCallback.jsx
+++ src/Pages/Components/AuthCallback.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./AuthCallback.css";
+import "./AuthCallBack.css";
 import question from "../../assets/question.png";
 import logo from "../../assets/logo.png";
 
